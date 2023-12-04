8a70f3fcc675eae0460437f194cfc2f69c1ab662
Gardimy Charles <gardimycharles889@gmail.com>
2023-12-04 17:34:45 -0500

    Update games_controller.rb file

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,36 @@
 class GamesController < ApplicationController
+  before_action :set_game, only: [:show, :edit, :update, :destroy]
+
   def index
     @games = Game.all
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    if @game.save
+      redirect_to games_path, notice: 'Game was successfully added.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @game.destroy
+    redirect_to games_path, notice: 'Game was successfully deleted.'
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:game).permit(:title, :description)
   end
 end
