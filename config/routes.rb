22f7ad047dc462f558b31e5d8f89a727a8479c60
Gardimy Charles <gardimycharles889@gmail.com>
2023-12-04 17:36:51 -0500

    Update the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  resources :games do
-    resources :reservations, only: [:index]
-  end
-  
-  resources :reservations, only: [:index]
-
-  root 'games#index'
+  resources :games
 end
