22cbd83a861389ac79f87f946198a04a2c573f46
Gardimy Charles <gardimycharles889@gmail.com>
2023-12-05 09:48:33 -0500

    Update the views gae

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,8 +3,6 @@ class GamesController < ApplicationController
 
   def index
     @games = Game.all
-	#puts "Number of games: #{@games.count}" 
-	#logger.debug "Games: #{@games.inspect}"
   end
 
   def new
