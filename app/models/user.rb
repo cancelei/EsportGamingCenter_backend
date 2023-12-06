3d0eaf40731917bddc416510d22451dd1039a606
Gardimy Charles <gardimycharles889@gmail.com>
2023-12-06 07:16:16 -0500

    Update user_controller.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
-class User < ApplicationRecord
-  has_many :reservations, dependent: :destroy
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    render json: @user
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
