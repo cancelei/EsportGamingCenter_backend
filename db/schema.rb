056556e7cf39b713a330745f751abfb88113be24
cancelei <glauber.bannwart@gmail.com>
2023-12-07 18:05:54 -0300

    Add game_id column to reservations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_07_023451) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_07_205655) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_07_023451) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "game_id"
   end
 
   create_table "users", force: :cascade do |t|
